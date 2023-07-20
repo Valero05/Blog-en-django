5bfe1da09e76640bb719ee2d1f6ae1041585f371
Valero05 <118315589+Valero05@users.noreply.github.com>
2023-07-20 16:45:28 -0400

    Delete Proyecto_Blog directory

--- Proyecto_Blog/blog/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Post
-
-class PostListInicioSerializer(ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = '__all__'
--- Proyecto_Blog/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('perfil/<int:pk>', views.Perfil, name='perfil'),
-    path('register', views.Register, name='registro'),
-]
