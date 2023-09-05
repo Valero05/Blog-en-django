098ec182012e9abe13c69ff07e7064306698f502
Valero05 <b.valero.r05@hotmail.com>
2023-09-04 21:59:42 -0300

    cambios

--- Proyecto_Blog/Proyecto_Blog/settings.py
+++ Proyecto_Blog/Proyecto_Blog/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'crispy_forms',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
